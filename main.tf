5fe69d956dcb50fa6761ecf97b8c8ccc1ce5ae38
Chris Doan <cdoan@us.ibm.com>
2018-10-05 15:53:35 -0500

    enable prefix name change

--- main.tf
+++ main.tf
@@ -2,22 +2,26 @@ module "master" {
   source       = "./modules/pebble"
   type = "master"
   count = 1
+  prefix = "${var.prefix}"
 }
 
 module "worker" {
   source       = "./modules/pebble"
+  prefix = "${var.prefix}"
   type = "worker"
   count = 1
 }
 
 module "management" {
   source       = "./modules/pebble"
+  prefix = "${var.prefix}"
   type = "management"
   count = 2
 }
 
 module "va" {
   source       = "./modules/pebble"
+  prefix = "${var.prefix}"
   type = "va"
   count = 1
   vcpu = 4
@@ -26,6 +30,7 @@ module "va" {
 
 module "boot" {
   source = "./modules/pebble"
+  prefix = "${var.prefix}"
   type = "boot"
   count = 1
 }
---
+++ variable.tf
@@ -0,0 +1,4 @@
+
+variable "prefix" {
+    default = "sre-byname"
+}
